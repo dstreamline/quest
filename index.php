cc9759480a5093c3558183926329fb5e2f33d038
dstreamline <dstreamline@gmail.com>
2013-12-09 00:57:59 +0200

    whatever

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <script src="http://code.jquery.com/jquery.min.js" type="text/javascript"></script>
-<script language="JavaScript" type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function () {
         $('.nums').keypress(function (e) {
             if (!(e.which == 8 || e.which == 44 || e.which == 45 || e.which == 46 || (e.which > 47 && e.which < 58))) return false;
@@ -57,72 +57,87 @@ if (isset($_GET['showMap'])) {
             }
         }
     </script>
-<div class="pb-10">
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
-        <p>
-            <select name="xType">
-                <option><?= $xType ?></option>
+    <div class="pb-10">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+            <p>
+                <select name="xType">
+                    <option><?= $xType ?></option>
+                    <?php
+                    if ("S" == $xType) {
+                        echo "<option>N</option>";
+                    } else {
+                        echo "<option>S</option>";
+                    };
+                    ?>
+                </select>
+                <input class="nums" maxlength="2" size="2" onFocus="doClear(this)" onBlur="doDefault(this)" name="xGrads"
+                       value='<?= $xGrads ?>'/>.
+                <input maxlength="6" size="6" name="xMins" value='<?= $xMins ?>' class="nums"/> N 49/50.~~~~
+            <p>
+                <select name="yType">
+                    <option><?= $yType ?></option>
+                    <?php
+                    if ("W" == $yType) {
+                        echo "<option>E</option>";
+                    } else {
+                        echo "<option>W</option>";
+                    };
+                    ?>
+                </select>
+                <input class='nums' maxlength="2" size="2" value='<?= $yGrads ?>' name="yGrads" onFocus="doClear(this)"
+                       onBlur="doDefault(this)" class="nums"/>.
+                <input maxlength="6" size="6" value='<?= $yMins ?>' name="yMins" class="nums"/> E 36.~~~~~~
+            <p>
+                <input type="submit" value="Locate"/>
+
+            <p>
+                <input type="checkbox" name="mobile" <?php if ("on" == $mobile) {
+                    echo "checked";
+                } ?> > wikimapia mobile version
+
+            <p>
+                <input type="checkbox" name="showMap" <?php if ("on" == $showMap) {
+                    echo "checked";
+                } ?> > show map picture
+
+            <div class="both">
+                <input type="text" id="adress" size="38">
                 <?php
-                if ("S" == $xType) {
-                    echo "<option>N</option>";
-                } else {
-                    echo "<option>S</option>";
-                };
-                ?>
-            </select>
-            <input class="nums" maxlength="2" size="2" onFocus="doClear(this)" onBlur="doDefault(this)" name="xGrads"
-                   value='<?= $xGrads ?>'/>.
-            <input maxlength="6" size="6" name="xMins" value='<?= $xMins ?>' class="nums"/> N 49/50.~~~~
-        <p>
-            <select name="yType">
-                <option><?= $yType ?></option>
-                <?php
-                if ("W" == $yType) {
-                    echo "<option>E</option>";
-                } else {
-                    echo "<option>W</option>";
-                };
-                ?>
-            </select>
-            <input class='nums' maxlength="2" size="2" value='<?= $yGrads ?>' name="yGrads" onFocus="doClear(this)"
-                   onBlur="doDefault(this)" class="nums"/>.
-            <input maxlength="6" size="6" value='<?= $yMins ?>' name="yMins" class="nums"/> E 36.~~~~~~
-        <p>
-            <input type="submit" value="Locate"/>
+                if (isset($_GET['xGrads']) && isset($_GET['xMins']) && isset($_GET['yGrads']) && isset($_GET['xMins'])) {
+                    if ($xType == "S") {
+                        $xGrads = -$xGrads;
+                    };
+                    $xCoor = "$xGrads.$xMins";
+                    if ($yType == "W") {
+                        $yGrads = -$yGrads;
+                    };
+                    $yCoor = "$yGrads.$yMins";
 
-        <p>
-            <input type="checkbox" name="mobile" <?php if ("on" == $mobile) {
-                echo "checked";
-            } ?> > wikimapia mobile version
 
-        <p>
-            <input type="checkbox" name="showMap" <?php if ("on" == $showMap) {
-                echo "checked";
-            } ?> > show map picture
+//                if ("on" == $mobile) {
+//                    echo "<a href=http://wikimapia.org/m/#lang=en&lat=" . $xCoor . "&lon=" . $yCoor . "&z=18&m=m target=\"_blank\"> <img src=\"http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG\" alt=Find by Wikimapia\" width=\"30\" height=\"30\"> </a><br>";
+//                } else {
+//                    echo "<a href=http://wikimapia.org/#lang=en&lat=" . $xCoor . "&lon=" . $yCoor . "&z=18&m=m target=\"_blank\"> <img src=\"http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG\" alt=Find by Wikimapia\" width=\"30\" height=\"30\"> </a><br>";
+//                }
 
-        <p>
-            <input type="text" id="adress" size="38">
-            <?php
-            if (isset($_GET['xGrads']) && isset($_GET['xMins']) && isset($_GET['yGrads']) && isset($_GET['xMins'])) {
-                if ($xType == "S") {
-                    $xGrads = -$xGrads;
-                };
-                $xCoor = "$xGrads.$xMins";
-                if ($yType == "W") {
-                    $yGrads = -$yGrads;
-                };
-                $yCoor = "$yGrads.$yMins";
-                if ("on" == $mobile) {
-                    echo "<a href=http://wikimapia.org/m/#lang=en&lat=" . $xCoor . "&lon=" . $yCoor . "&z=18&m=m target=\"_blank\"> <img src=\"http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG\" alt=Find by Wikimapia\" width=\"30\" height=\"30\"> </a><br>";
-                } else {
-                    echo "<a href=http://wikimapia.org/#lang=en&lat=" . $xCoor . "&lon=" . $yCoor . "&z=18&m=m target=\"_blank\"> <img src=\"http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG\" alt=Find by Wikimapia\" width=\"30\" height=\"30\"> </a><br>";
+                if ("on" == $mobile):?>
+                <a href=http://wikimapia.org/m/#lang=en&lat=<?php echo $xCoor; ?>&lon=<?php echo $yCoor; ?>&z=18&m=m target="_blank"> <img src="http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG" alt="Find by Wikimapia" width="30" height="30"> </a><br>
+                    <?php else: ?>
+                    <a href=http://wikimapia.org/#lang=en&lat=<?php echo $xCoor; ?>&lon=<?php echo $yCoor; ?>&z=18&m=m target="_blank"> <img src="http://upload.wikimedia.org/wikipedia/commons/e/ed/WikimapiaLogo.PNG" alt="Find by Wikimapia" width="30" height="30"> </a><br>
+                            <?php endif;
+
+
+
+
+
+
+                     if ("on" == $showMap) {
+                        echo "<img class =\"yandex-image-size\" src=\"http://static-maps.yandex.ru/1.x/?ll=" . $yCoor . "," . $xCoor . "&spn=0.012657,0.00189&pt=" . $yCoor . "," . $xCoor . ",pm2ntl&l=map\">";
+                    }
                 }
-                if ("on" == $showMap) {
-                    echo "<img src=\"http://static-maps.yandex.ru/1.x/?ll=" . $yCoor . "," . $xCoor . "&spn=0.012657,0.00189&pt=" . $yCoor . "," . $xCoor . ",pm2ntl&l=map\">";
-                }
-            }
-            ?>
-    </form>
+                ?>
+            </div>
+        </form>
     </div>
     <?php
     ?>
@@ -144,8 +159,8 @@ if (isset($_GET['showMap'])) {
     </script>
     <a href="index.php">Reset</a>
 
-    <p>
-        v 1.0
+
+    v 1.0
 </div>
 </body>
 </html>
