0144b1193a70d09da8a71ef3671a73eef6f15249
galaxyipcom <galaxyipcom@gmail.com>
2013-12-10 22:56:09 +0200

    initial

---
+++ protected/controllers/HelperController.php
@@ -0,0 +1,17 @@
+<?php
+
+class HelperController extends Controller
+{
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+        echo 1;
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		$this->renderPartial('index');
+	}
+}
