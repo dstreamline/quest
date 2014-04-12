8ecc40a83882733bd5891241ad47374ed915f568
dstreamline <dstreamline@gmail.com>
2014-04-12 16:18:25 +0300

    minor changes

--- protected/views/form/results.php
+++ protected/views/form/results.php
@@ -1,13 +1,13 @@
-<div>
+<div class="row code-wrapper"><div class="span12">
     <?php
     foreach ($model as $key => $cell):?>
-        <div class="blah" style="float:left; border: solid #000000 1px" id="<?php echo 'cell_id' . $cell->id ?>"
-             class="<?php echo ($cell->checked == 0) ? 'none' : 'Ok'; ?>">
+        <div class="code btn " style="float:left" id="<?php echo 'cell_id' . $cell->id ?>"
+             class="<?php echo ($cell->checked == 0) ? 'none' : 'btn-inverse'; ?>">
             <?php echo $cell->cell_value; ?>
         </div>
     <?php endforeach; ?>
 </div>
-
+</div>
 
 <script type="text/javascript">
 
@@ -15,19 +15,20 @@ $('.blah').click(function(){
     $.post( "<?php echo $this->createUrl('form/updatecell', array('id'=>$this->id));?>",
         { id: $(this).id})
         .done(function( data ) {}, "json");
-})
+});
 
     function autoUpdated(){
         $.post( "<?php echo $this->createUrl('form/updater', array('id'=>$this->id));?>",
             { id: "<?php echo $geoModel->id;?>"})
             .done(function( data ) {
                 $.each(JSON.parse(data), function(index, value) {
+
                     console.log(value);
                     console.log(index);
                 });
             }, "json");
     }
 
-    setInterval(autoUpdated,5000);
+    setInterval(autoUpdated,3000);
 
     </script>
